35640bc3e12663d37ceffffb1f4aa7e83cdab335
Johnson <jo74705@gmail.com>
2021-02-04 00:32:05 +0800

    rename repo

--- android/app/src/main/java/com/johnsonsy/prettyface/MainActivity.java
+++ android/app/src/main/java/com/johnsonsy/prettyface/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hoja;
+package com.johnsonsy.prettyface;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Hoja";
+    return "prettyFace";
   }
 }
--- ios/prettyFaceTests/prettyFaceTests.m
+++ ios/prettyFaceTests/prettyFaceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HojaTests : XCTestCase
+@interface prettyFaceTests : XCTestCase
 
 @end
 
-@implementation HojaTests
+@implementation prettyFaceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
